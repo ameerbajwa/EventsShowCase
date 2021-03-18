30b9cfb212286678faf59eb81752f9def83614c7
Ameer Bajwa <ameerbajwa@gmail.com>
2021-03-17 19:05:22 -0700

    first commit

---
+++ EventsShowCase/EventsShowCase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventsShowCase
+//
+//  Created by Ameer Bajwa on 3/17/21.
+//  Copyright © 2021 Ameer Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
